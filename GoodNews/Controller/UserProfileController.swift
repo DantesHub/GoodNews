7e1105bc497cf7fe60272dbc12b2a2b6033ae2fe
Raymond Yoo <rayyoo4223@gmail.com>
2020-04-26 11:03:58 -0400

    added twitter ui

--- GoodNews/Controller/UserProfileController.swift
+++ GoodNews/Controller/UserProfileController.swift
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import UIKit
 import RealmSwift
 
@@ -20,14 +18,14 @@ class UserProfileController: UIViewController {
         super.viewDidLoad()
         onHome = false
         navigationController?.navigationBar.backgroundColor = .white
-        navigationController?.navigationBar.tintColor = .white
+    navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.barTintColor = .white
         navigationController?.navigationBar.barStyle = .black
         navigationController?.navigationBar.titleTextAttributes =
-        [NSAttributedString.Key.foregroundColor: UIColor.black,
-        NSAttributedString.Key.font: UIFont(name: "Menlo-Bold", size: 30)!]
-//        UINavigationBar.appearance().shadowImage = UIImage()
-//        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+    [NSAttributedString.Key.foregroundColor: UIColor.black,
+                 NSAttributedString.Key.font: UIFont(name: "Menlo-Bold", size: 30)!]
+//        UINavigationBar.appearance().shadowImage = UIImage()
+//        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.isTranslucent = true;
 
         configureUI()
@@ -35,15 +33,15 @@ class UserProfileController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(false, animated: true) //SHow
+    self.navigationController?.setNavigationBarHidden(false, animated: true) //SHow
 
-        print("welcome screen")
-        results = uiRealm.objects(User.self)
-        for result in results {
-            if result.isLoggedIn == true {
-                navigationController?.pushViewController(SettingsController(), animated: false)
-            }
+    print("welcome screen")
+    results = uiRealm.objects(User.self)
+    for result in results {
+    if result.isLoggedIn == true {
+        navigationController?.pushViewController(SettingsController(), animated: false)
         }
+    }
     }
     //MARK: - Helper Functions
     func configureUI() {
@@ -58,60 +56,60 @@ class UserProfileController: UIViewController {
         view.backgroundColor = .white
         titleLabel1.frame.size.width = 300
         titleLabel1.frame.size.height = 100
-        titleLabel1.center.x = view.center.x
-        titleLabel1.center.y = view.center.y - 200
-        titleLabel1.text = "GOOD"
+    titleLabel1.center.x = view.center.x
+    titleLabel1.center.y = view.center.y - 200
+    titleLabel1.text = "GOOD"
         titleLabel1.font = UIFont(name: "Menlo", size: 55)
         titleLabel1.textColor = goldYellow
-        
-        titleLabel2.frame.size.width = 300
-        titleLabel2.frame.size.height = 100
-        titleLabel2.center.x = view.center.x + 145
-        titleLabel2.center.y = view.center.y - 200
+    
+    titleLabel2.frame.size.width = 300
+    titleLabel2.frame.size.height = 100
+    titleLabel2.center.x = view.center.x + 145
+    titleLabel2.center.y = view.center.y - 200
         titleLabel2.text = "NEWS!"
-        titleLabel2.font = UIFont(name: "Menlo", size: 55)
+    titleLabel2.font = UIFont(name: "Menlo", size: 55)
         titleLabel2.textColor = goldYellow
-        
-        middleText.frame.size.width = 300
+    
+    middleText.frame.size.width = 300
         middleText.frame.size.height = 400
         middleText.center.x = view.center.x
         middleText.center.y = view.center.y - 50
-        middleText.text = "Do you want the good news or the good news first?"
+    middleText.text = "Do you want the good news or the good news first?"
         middleText.numberOfLines = 0;
         middleText.font = UIFont(name: "Hiragino Sans", size: 40)
         
-        registerView =  UIView(frame: CGRect(x: 100, y: 400, width: 350, height: 80))
+        registerView =  UIView(frame: CGRect(x: 100, y: 400, width: 350, height: 80))
         registerView.applyDesign(color: bookmarkRed)
         registerView.center.x = view.center.x
-        registerView.center.y = view.center.y + 250
-        let tap = UITapGestureRecognizer(target: self, action: #selector(tappedRegister))
-        registerView.addGestureRecognizer(tap)
-        
-        loginView =  UIView(frame: CGRect(x: 100, y: 400, width: 350, height: 80))
+    registerView.center.y = view.center.y + 250
+    let tap = UITapGestureRecognizer(target: self, action: #selector(tappedRegister))
+    registerView.addGestureRecognizer(tap)
+    
+    loginView =  UIView(frame: CGRect(x: 100, y: 400, width: 350, height: 80))
         loginView.applyDesign(color: twitterBlue)
-        loginView.center.x = view.center.x
+    loginView.center.x = view.center.x
         loginView.center.y = view.center.y + 140
         let tap2 = UITapGestureRecognizer(target: self, action: #selector(tappedLogin))
         loginView.addGestureRecognizer(tap2)
         
         registerLabel.applyDesign(text: "Register")
-        registerLabel.sizeToFit()
-        registerLabel.center.x = view.center.x
+    registerLabel.sizeToFit()
+    registerLabel.center.x = view.center.x
         registerLabel.center.y = view.center.y + 250
+    
         
-        
-        loginLabel.applyDesign(text: "Login")
+    loginLabel.applyDesign(text: "Login")
         loginLabel.sizeToFit()
-        loginLabel.center.x = view.center.x
+    loginLabel.center.x = view.center.x
         loginLabel.center.y = view.center.y + 140
+    
         
-        
-        view.addSubview(titleLabel1)
+    view.addSubview(titleLabel1)
         view.addSubview(titleLabel2)
         view.addSubview(loginView)
-        view.addSubview(registerView)
+    view.addSubview(registerView)
         view.addSubview(loginLabel)
-        view.addSubview(registerLabel)
+    view.addSubview(registerLabel)
         view.addSubview(middleText)
     }
     
@@ -123,129 +121,8 @@ class UserProfileController: UIViewController {
     }
 
     @objc func tappedRegister() {
-        print("tapped")
-        let registerVC = RegisterViewController()
-        self.navigationController?.pushViewController(registerVC, animated: false)
+    print("tapped")
+    let registerVC = RegisterViewController()
+    self.navigationController?.pushViewController(registerVC, animated: false)
     }
 }
-
-
-=======
-//import UIKit
-//
-//class UserProfileController: UIViewController {
-//    //MARK: - Properties
-//    var loginView = UIView()
-//    var registerView = UIView()
-//    var loginLabel = UILabel()
-//    var registerLabel = UILabel()
-//    let titleLabel1 = UILabel()
-//    let titleLabel2 = UILabel()
-//    let middleText = UILabel()
-//
-//    //MARK: - INIT
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    onHome = false
-//        navigationController?.navigationBar.backgroundColor = .white
-//        navigationController?.navigationBar.tintColor = .white
-//        navigationController?.navigationBar.barTintColor = .white
-//    navigationController?.navigationBar.barStyle = .black
-//        navigationController?.navigationBar.titleTextAttributes =
-//            [NSAttributedString.Key.foregroundColor: UIColor.black,
-//                 NSAttributedString.Key.font: UIFont(name: "Menlo-Bold", size: 30)!]
-////        UINavigationBar.appearance().shadowImage = UIImage()
-////        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-//        navigationController?.navigationBar.isTranslucent = true;
-//
-//        configureUI()
-//
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//    self.navigationController?.setNavigationBarHidden(false, animated: true) //SHow
-//    }
-//    //MARK: - Helper Functions
-//    func configureUI() {
-//        view.backgroundColor = .white
-//    navigationController?.navigationBar.prefersLargeTitles = true
-//    navigationItem.title = "Settings"
-//    loadComponents()
-//    }
-//
-//    //MARK: - Handlers
-//    func loadComponents() {
-//        view.backgroundColor = .white
-//    titleLabel1.frame.size.width = 300
-//        titleLabel1.frame.size.height = 100
-//    titleLabel1.center.x = view.center.x
-//        titleLabel1.center.y = view.center.y - 200
-//    titleLabel1.text = "GOOD"
-//        titleLabel1.font = UIFont(name: "Menlo", size: 55)
-//        titleLabel1.textColor = goldYellow
-//
-//    titleLabel2.frame.size.width = 300
-//        titleLabel2.frame.size.height = 100
-//    titleLabel2.center.x = view.center.x + 145
-//        titleLabel2.center.y = view.center.y - 200
-//    titleLabel2.text = "NEWS!"
-//        titleLabel2.font = UIFont(name: "Menlo", size: 55)
-//    titleLabel2.textColor = goldYellow
-//
-//        middleText.frame.size.width = 300
-//        middleText.frame.size.height = 400
-//        middleText.center.x = view.center.x
-//        middleText.center.y = view.center.y - 50
-//        middleText.text = "Do you want the good news or the good news first?"
-//    middleText.numberOfLines = 0;
-//        middleText.font = UIFont(name: "Hiragino Sans", size: 40)
-//
-//        registerView =  UIView(frame: CGRect(x: 100, y: 400, width: 350, height: 80))
-//        registerView.applyDesign(color: bookmarkRed)
-//    registerView.center.x = view.center.x
-//    registerView.center.y = view.center.y + 250
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(tappedRegister))
-//    registerView.addGestureRecognizer(tap)
-//
-//        loginView =  UIView(frame: CGRect(x: 100, y: 400, width: 350, height: 80))
-//        loginView.applyDesign(color: twitterBlue)
-//        loginView.center.x = view.center.x
-//        loginView.center.y = view.center.y + 140
-////        let tap2 = UITapGestureRecognizer(target: self, action: #selector(tappedLogin))
-////        loginView.addGestureRecognizer(tap2)
-//
-//    registerLabel.applyDesign(text: "Register")
-//        registerLabel.sizeToFit()
-//    registerLabel.center.x = view.center.x
-//        registerLabel.center.y = view.center.y + 250
-//
-//
-//        loginLabel.applyDesign(text: "Login")
-//        loginLabel.sizeToFit()
-//        loginLabel.center.x = view.center.x
-//    loginLabel.center.y = view.center.y + 140
-//
-//
-//        view.addSubview(titleLabel1)
-//    view.addSubview(titleLabel2)
-//        view.addSubview(loginView)
-//    view.addSubview(registerView)
-//        view.addSubview(loginLabel)
-//    view.addSubview(registerLabel)
-//        view.addSubview(middleText)
-//    }
-//
-//
-//    //MARK: - Handlers
-////    @objc func tappedLogin() {
-////        let loginVC = LoginViewController()
-////        self.navigationController?.pushViewController(loginVC, animated: false)
-////    }
-////
-//    @objc func tappedRegister() {
-//    print("tapped")
-//    let registerVC = RegisterViewController()
-//    self.navigationController?.pushViewController(registerVC, animated: false)
-//    }
-//}
->>>>>>> 49cb59c4c0edeeda307f43201cff65832e9e7e00
---
+++ GoodNews/View/TweetCell.swift
@@ -0,0 +1,178 @@
+//
+//  TweetCell.swift
+//  GoodNews
+//
+//  Created by Raymond Yoo on 4/26/20.
+//  Copyright © 2020 Dante Kim. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UICollectionViewCell{
+    
+    // MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = UIColor(hexString: "#00acee")
+        return iv
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "comment"), for: .normal)
+        button.tintColor = .darkGray
+        button.setDimensions(width: 20, height: 20)
+        return button
+    }()
+    private lazy var retweetButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setImage(UIImage(named: "retweet"), for: .normal)
+           button.tintColor = .darkGray
+           button.setDimensions(width: 20, height: 20)
+           return button
+    }()
+    private lazy var likeButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setImage(UIImage(named: "like"), for: .normal)
+           button.tintColor = .darkGray
+           button.setDimensions(width: 20, height: 20)
+           return button
+    }()
+    private lazy var shareButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setImage(UIImage(named: "share"), for: .normal)
+           button.tintColor = .darkGray
+           button.setDimensions(width: 20, height: 20)
+           return button
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.text = "some text caption"
+        return label
+    }()
+    
+    private let infoLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,
+                                paddingTop: 8, paddingLeft: 8)
+        
+        let stack = UIStackView(arrangedSubviews: [infoLabel, captionLabel])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.anchor(top: profileImageView.topAnchor, left: profileImageView.rightAnchor, right: rightAnchor, paddingLeft: 12, paddingRight: 12)
+        
+        infoLabel.font = UIFont.systemFont(ofSize: 14)
+        infoLabel.text = "Eddie Brock @venom"
+        
+        let actionStack = UIStackView(arrangedSubviews: [commentButton, retweetButton, likeButton, shareButton])
+        actionStack.axis = .horizontal
+        actionStack.spacing = 72
+        
+        addSubview(actionStack)
+        actionStack.centerX(inView: self)
+        actionStack.anchor(bottom: bottomAnchor, paddingBottom: 8)
+        
+        let underlineView = UIView()
+        underlineView.backgroundColor = .systemGroupedBackground
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor,
+                             right: rightAnchor, height: 1)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
+extension UIView {
+    func setDimensions(width: CGFloat, height: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(inView view: UIView, topAnchor: NSLayoutYAxisAnchor? = nil, paddingTop: CGFloat? = nil) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        if let topAnchor = topAnchor, let padding = paddingTop {
+            self.topAnchor.constraint(equalTo: topAnchor, constant: padding).isActive = true
+        }
+    }
+    
+    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil, paddingLeft: CGFloat? = nil, constant: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant!).isActive = true
+        if let leftAnchor = leftAnchor, let padding = paddingLeft {
+            self.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+        }
+    }
+}
